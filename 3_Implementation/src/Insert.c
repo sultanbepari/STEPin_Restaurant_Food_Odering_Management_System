09128dd6805b9e21530450aef351c1ada0f562f3
sultanbepari <53936397+sultanbepari@users.noreply.github.com>
2021-09-07 18:03:34 +0530

    Update Insert.c

--- 3_Implementation/src/Insert.c
+++ 3_Implementation/src/Insert.c
@@ -1,13 +1,13 @@
 #include "header.h"
 #include "Initilization.h"
 #include<string.h>
-int Add_Item(top *t, float Data, char Item_name[25], int quant, float Amount)
+int Add_Item(top *t, float Data, char Item_name[25], int quant, float AMT)
 {
     Meals *p, *q;
     p=(Meals *)malloc(sizeof(Meals));
     p->Data=Data;
     p->quant=quant;
-    p->Amount=Amount;
+    p->AMT=Amount;
     strcpy(p->Item_name, Item_name);
     p->next=NULL;
     if(t->start==NULL)
