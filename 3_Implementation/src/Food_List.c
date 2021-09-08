78fb80d673b3f64024778d6eb4166bede1f94bec
sultanbepari <53936397+sultanbepari@users.noreply.github.com>
2021-09-08 11:55:38 +0530

    Update Food_List.c

--- 3_Implementation/src/Food_List.c
+++ 3_Implementation/src/Food_List.c
@@ -5,22 +5,22 @@
 int Item_List(top *t)
 {
 
-	printf("\n\t"); //console_color(240);
-	printf("_______________________________________________________");//console_color(26);
-	printf("\n\t");// console_color(240);
-	printf("  Food Code          Name                Price         ");//console_color(26);
-	printf("\n\t"); //console_color(240);
-	printf("-------------------------------------------------------");//console_color(26);
+// 	printf("\n\t"); //console_color(240);
+// 	printf("_______________________________________________________");//console_color(26);
+// 	printf("\n\t");// console_color(240);
+// 	printf("  Food Code          Name                Price         ");//console_color(26);
+// 	printf("\n\t"); //console_color(240);
+// 	printf("-------------------------------------------------------");//console_color(26);
 
 	Meals *p;
     p=t->start;
     while(p!= NULL)
     {
-        printf("\n\t");
-        printf("  %0.2f   |  %-25s     |    %0.2f   ",p->Data,p->Item_name,p->Amount);
-		//console_color(26);
-		printf("\n\t");
-        printf("-------------------------------------------------------");
+//         printf("\n\t");
+//         printf("  %0.2f   |  %-25s     |    %0.2f   ",p->Data,p->Item_name,p->Amount);
+// 		//console_color(26);
+// 		printf("\n\t");
+//         printf("-------------------------------------------------------");
         p=p->next;
     }
     return 0;
