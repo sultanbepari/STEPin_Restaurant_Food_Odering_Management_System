b72729900c310edffcf13d5d3671894cc34bef91
sultanbepari <53936397+sultanbepari@users.noreply.github.com>
2021-09-08 11:56:35 +0530

    Update Previous_Order.c

--- 3_Implementation/src/Previous_Order.c
+++ 3_Implementation/src/Previous_Order.c
@@ -4,21 +4,21 @@
 
 int Previous_Order(top *t)
 {
-	printf("\n\t"); //console_color(240);
-	printf("___________________________________________________________________");//console_color(26);
-	printf("\n\t"); //console_color(240);
-	printf("  Food Code       Name                       Quantity      Price   ");//console_color(26);
-	printf("\n\t");// console_color(240);
-	printf("-------------------------------------------------------------------");//console_color(26);
+// 	printf("\n\t"); //console_color(240);
+// 	printf("___________________________________________________________________");//console_color(26);
+// 	printf("\n\t"); //console_color(240);
+// 	printf("  Food Code       Name                       Quantity      Price   ");//console_color(26);
+// 	printf("\n\t");// console_color(240);
+// 	printf("-------------------------------------------------------------------");//console_color(26);
 
 	Meals *p;
     p=t->start;
     while(p!= NULL)
     {
-		printf("\n\t");
-        printf("  %0.2f   |  %-25s      |    %2d      |  %0.2f   ",p->Data,p->Item_name, p->quant, p->Amount);
-		printf("\n\t");
-		printf("------------------------------------------------------------------");
+// 		printf("\n\t");
+//         printf("  %0.2f   |  %-25s      |    %2d      |  %0.2f   ",p->Data,p->Item_name, p->quant, p->Amount);
+// 		printf("\n\t");
+// 		printf("------------------------------------------------------------------");
         p=p->next;
     }
     return 0;
